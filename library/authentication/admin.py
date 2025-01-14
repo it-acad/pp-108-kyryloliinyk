9188d9289c481e9478423818cc03715eadbc8849
kyryloliinyk <kyryloliinyk@gmail.com>
2025-01-14 15:19:25 +0200

    Update user admin list page

--- library/authentication/admin.py
+++ library/authentication/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import CustomUser
 
-@admin.register(CustomUser)
+
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'first_name', 'last_name')
-    search_fields = ('email', 'first_name', 'last_name')
+    list_display = ('first_name', 'last_name', 'email', 'role')
+    search_fields = ('email', 'first_name', 'last_name', 'email')
+    list_filter = ('role',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
