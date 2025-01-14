4ab9752628afad779b7c4f8576a50c3c08f21ba0
kyryloliinyk <kyryloliinyk@gmail.com>
2025-01-14 17:03:11 +0200

    Form a visual distribution of information about Books
    so that there is a separate section with data that do not change

--- library/book/admin.py
+++ library/book/admin.py
@@ -2,12 +2,24 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'count', 'author_list')
+    list_display = ('name', 'author_list', 'count')
     search_fields = ('name', 'description')
     list_filter = ('count', 'authors')
 
+    # Mark `name` and `author_list` as read-only fields
+    readonly_fields = ('name', 'author_list')
+
     def author_list(self, obj):
-        return ", ".join([author.__str__() for author in obj.authors.all()])
+        return ", ".join([str(author) for author in obj.authors.all()])
     author_list.short_description = 'Authors'
+
+    fieldsets = (
+        ('Immutable Data', {  # Section for data that doesn't change
+            'fields': ('name', 'author_list')  # Both are read-only
+        }),
+        ('Mutable Data', {  # Section for data that changes
+            'fields': ('count',)
+        }),
+    )
 
 admin.site.register(Book, BookAdmin)
