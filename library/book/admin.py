58f9417bb9d93ff40a079b07aa7ce39b14b18ad0
kyryloliinyk <kyryloliinyk@gmail.com>
2025-01-14 14:42:04 +0200

    Update Books in admin panel
    * add list view
    * add filters
    * add search bar

--- library/book/admin.py
+++ library/book/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'count')
+    search_fields = ('name', 'description')
+    list_filter = ('count', 'authors')
+
+admin.site.register(Book, BookAdmin)
+
