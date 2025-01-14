d531bbcdba6bf38c9a25df078d6bd99f33210ba6
kyryloliinyk <kyryloliinyk@gmail.com>
2025-01-14 20:33:29 +0200

    Make immutable fields on book edit page not editable

--- library/book/admin.py
+++ library/book/admin.py
@@ -1,25 +1,49 @@
 from django.contrib import admin
 from .models import Book
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author_list', 'count')
-    search_fields = ('name', 'description')
-    list_filter = ('count', 'authors')
+class DistinctListFilter(admin.SimpleListFilter):
+    title = 'Publication Year'
+    parameter_name = 'publication_year'
 
-    # Mark `name` and `author_list` as read-only fields
-    readonly_fields = ('name', 'author_list')
+    def lookups(self, request, model_admin):
+        unique_years = Book.objects.order_by('publication_year').values_list('publication_year', flat=True).distinct()
+        return [(year, year) for year in unique_years if year is not None]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(publication_year=self.value())
+        return queryset
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author_list', 'count', 'publication_year')
+    search_fields = ('name', 'description')
+    list_filter = ('count', DistinctListFilter)
 
     def author_list(self, obj):
         return ", ".join([str(author) for author in obj.authors.all()])
     author_list.short_description = 'Authors'
 
-    fieldsets = (
-        ('Immutable Data', {  # Section for data that doesn't change
-            'fields': ('name', 'author_list')  # Both are read-only
-        }),
-        ('Mutable Data', {  # Section for data that changes
-            'fields': ('count',)
-        }),
-    )
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # Edit form
+            return ('name', 'author_list', 'publication_year')
+        return ()
+
+    def get_fieldsets(self, request, obj=None):
+        if obj:  # Edit form
+            return (
+                ('Immutable Data', {
+                    'fields': ('name', 'author_list', 'publication_year'),
+                }),
+                ('Mutable Data', {
+                    'fields': ('description', 'count', 'last_issued_date'),
+                }),
+            )
+        else:  # Create form
+            return (
+                ('Create Book', {
+                    'fields': ('name', 'description', 'count', 'publication_year'),
+                }),
+            )
 
 admin.site.register(Book, BookAdmin)
